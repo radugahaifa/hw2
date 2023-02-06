8108960f43f423ea346ddf29700b0dcbbdfd4a38
andrey raduga <radugahaifa@gmail.com>
2023-02-05 22:03:11 -0500

    proba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void hw2(String[] args) {
 
         int age = 18;
         if (age >= 18) {
@@ -32,21 +32,57 @@ public class Main {
 
                     int vozrast2 = 12;
                     boolean needGoToSchool = vozrast2 > 7 || vozrast2 < 18;
-                    if(needGoToSchool){
-                        System.out.println(" you have to go to school");
+                    if (needGoToSchool) {
+                        System.out.println("if " + needGoToSchool + " you have to go to school");
                         int vozrast3 = 21;
-                        boolean needGoToUniversity = vozrast3>18 || vozrast3<24;
-                        if(needGoToUniversity){
+                        boolean needGoToUniversity = vozrast3 > 18 || vozrast3 < 24;
+                        if (needGoToUniversity) {
                             System.out.println("you have to go to university");
                         }
-                    }
-                }
-            }
+                    }//task 5
 
-        }
+                    int kidage = 5;
+                    if (kidage < 5) ;
+                    System.out.println("you can't ride the carousel");
 
-    }
+                    int kidage2 = 10;
+                    if (kidage2 >= 5 && kidage2 <= 14) ;
+                    {
+                        System.out.println("you can ride with adults, but you can't without adults");
+
+                        if (kidage2 > 14) ;
+                        System.out.println("you can ride the carousel");
+
+                        //task 6
 
 
-}
+                        int totalSeats = 102;
+                        int seating = 60;
+                        int standing = (totalSeats - seating);
+                        if (totalSeats < seating + standing) ;{
+                        System.out.println("there are still same places");
+                    }else{
+                        System.out.println("there are not seats");
+
+                        //task 7
+
+
+                        int one = 1;
+                        int two = 2;
+                        int three = 3;
+                        if (three > one && three > two) ;{
+                        System.out.println("three is biggest number");
+
+                    }}
+
+
+
+
+
+
+
+
+
+
+
 
